2ccca95a294a93c08aaa7eff1412b710059941b3
schuckman <charlieschuck19@gmail.com>
2021-11-13 11:35:05 -0500

    Added GUI

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socket
+#pip install pysimplegui
+import PySimpleGUI as gui
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # (IPv4, TCP)
 
@@ -10,8 +12,25 @@ except socket.error as e:
 serverOut = s.recv(1024)
 
 while True:
-    lstring = input("enter a word.")
+    layout = [[gui.Text('Enter a Word: ', size = (15, 1)), gui.InputText()], [gui.Submit()], [gui.Button("Cancel")]]
+    window = gui.Window('Word Window', layout)
+    event, values = window.read()
+    if event == "Cancel" or event == gui.WIN_CLOSED:
+        break
+
+    lstring = str(values[0])
+    window.close()
+
     s.send(bytes(lstring, "utf-8"))
     serverOut = s.recv(8192)
-    print(serverOut.decode('utf-8'))
+
+    layout = [[gui.Text(serverOut.decode('utf-8'))], [gui.Button("OK")]]
+
+    window = gui.Window("Definition", layout)
+
+    while True:
+        event, values = window.read()
+        if event == "OK" or event == gui.WIN_CLOSED:
+            break
+    window.close()
 s.close()
